cb72afea9a117cadc240832412406baf0af92fc2
Insouciant21 <48847364+Insouciant21@users.noreply.github.com>
2020-08-22 22:43:25 +0800

    2020/8/22 Solutions

---
+++ 普及-/P1125/P1125.cpp
@@ -0,0 +1,32 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+bool validate(int x) {
+	if (x < 2) return false;
+	for (int i = 2; i <= sqrt(x); i++)
+		if (x % i == 0) return false;
+	return true;
+}
+
+int main() {
+	string x;
+	cin >> x;
+	vector<int>cnt;
+	int t;
+	for (char i = 'a'; i <= 'z'; i++) {
+		t = count(x.begin(), x.end(), i);
+		if (t != 0)
+			cnt.push_back(t);
+	}
+	sort(cnt.begin(), cnt.end());
+	if (validate(cnt.back() - cnt.front())) {
+		cout << "Lucky Word" << endl;
+		cout << cnt.back() - cnt.front();
+	}
+	else {
+		cout << "No Answer" << endl;
+		cout << 0 << endl;
+	}
+	return 0;
+}
---
+++ 普及-/P1598/P1598.cpp
@@ -0,0 +1,45 @@
+﻿/*
+*	Problem: P1598
+*	Author: Insouciant21
+*	Time: 2020/8/22 20:37:45
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int fre[26];
+string a, b, c, d;
+string s;
+
+int main() {
+    getline(cin, a);
+    getline(cin, b);
+    getline(cin, c);
+    getline(cin, d);
+    s = a + b + c + d;
+    for (char i = 'A'; i <= 'Z'; i++) {
+        fre[i - 65] = count(s.begin(), s.end(), i);
+    }
+    int n = *(max_element(begin(fre), end(fre)));
+    vector<vector<string> > histo(n + 1, vector<string>(26));
+    for (int i = 0; i < 26; i++) {
+        for (int j = 0; j < n + 1; j++) {
+            histo[j][i] = " ";
+        }
+    }
+    for (int i = 0; i < 26; i++) 
+        histo[n][i] = char(i + 65);
+    for (int i = 0; i < 26; i++) {
+        for (int j = n - 1; j >= n - fre[i]; j--) {
+            histo[j][i] = "*";
+        }
+    }
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j < 26; j++) {
+            (j != 0) ? cout << " " << histo[i][j] : cout << histo[i][j];
+        }
+        cout << endl;
+    }
+    return 0;
+}
