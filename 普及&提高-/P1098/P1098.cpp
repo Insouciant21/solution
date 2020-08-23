fcd519b8bfe8892ff4284f0e5172c4698e94807f
Insouciant21 <48847364+Insouciant21@users.noreply.github.com>
2020-08-23 16:27:05 +0800

    2020/8/23

---
+++ 普及&提高-/P1098/P1098.cpp
@@ -0,0 +1,89 @@
+﻿/*
+*	Problem: P1098
+*	Author: Insouciant21
+*	Time: 2020/8/23 13:28:25
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool validate(char x, char y) {
+	if ('0' <= x && x <= '9' && '0' <= y && y <= '9') return 1;
+	else if ('a' <= x && x <= 'z' && 'a' <= y && y <= 'z') return 1;
+	else if ('A' <= x && x <= 'Z' && 'A' <= y && y <= 'Z') return 1;
+	return 0;
+}
+
+int typeof(char x) {
+	if (x >= 'a' && x <= 'z') return 0;
+	if (x >= 'A' && x <= 'Z') return 1;
+	if (x >= '0' && x <= '9') return 2;
+}
+
+string generate(string x, int a, int b, int c) {
+	string ans;
+	char filled;
+	for (char i = x.front() + 1; i < x.back(); i++) {
+		switch (typeof(i))
+		{
+		case 0:
+			if (a == 1) filled = i;
+			else if (a == 2) filled = toupper(i);
+			else if (a == 3) filled = '*';
+			break;
+		case 1:
+			if (a == 1) filled = tolower(i);
+			else if (a == 2) filled = i;
+			else if (a == 3) filled = '*';
+			break;
+		case 2:
+			if (a != 3) filled = i;
+			else filled = '*';
+			break;
+		default:
+			filled = i;
+			break;
+		}
+		for (int j = 1; j <= b; j++) {
+			ans += filled;
+		}
+	}
+	if (c == 2) reverse(ans.begin(), ans.end());
+	stringstream ss;
+	ss << x.front() << ans << x.back();
+	ss >> ans;
+	return ans;
+}
+
+int main() {
+	int p1, p2, p3;
+	cin >> p1 >> p2 >> p3;
+	string pen;
+	cin >> pen;
+	size_t pos = 0;
+	string replace, origin;
+	stringstream ss;
+	size_t f = pen.find_first_not_of('-'), b = pen.find_last_not_of('-');
+	string fr = pen.substr(0, f);
+	string bk = pen.substr(b + 1, pen.length());
+	pen.erase(0, f);
+	pen.erase(b + 1, pen.length());
+	while (pen.find('-', pos) != string::npos) {
+		pos = pen.find('-', pos);
+		if (pos == 0 || pos == pen.length() - 1) continue;
+		if (pen[pos - 1] < pen[pos + 1]) {
+			if (validate(pen[pos - 1], pen[pos + 1])) {
+				ss << pen[pos - 1] << pen[pos] << pen[pos + 1];
+				ss >> origin;
+				ss.clear();
+				replace = generate(origin, p1, p2, p3);
+				pen.erase(pos - 1, 3);
+				pen.insert(pos - 1, replace);
+			}
+		}
+		pos++;
+	}
+	cout << fr << pen << bk << endl;
+	return 0;
+}
---
+++ 普及-/P1042/P1042.cpp
@@ -0,0 +1,45 @@
+﻿/*
+*	Problem: P1042
+*	Author: Insouciant21
+*	Time: 2020/8/23 15:59:37
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string>result_11;
+vector<string>result_21;
+char t;
+
+int main() {
+	long long cnt = 1;
+	cin >> t;
+	long long hua_11 = 0, ene_11 = 0;
+	long long hua_21 = 0, ene_21 = 0;
+	if (t == 'W') { hua_11++; hua_21++; }
+	else { ene_11++; ene_21++; }
+	while (t != 'E') {
+		cin >> t;
+		cnt++;
+		if (t == 'W') { hua_11++; hua_21++; }
+		else if (t == 'L') { ene_11++; ene_21++; }
+		if (hua_11 + ene_11 >= 11 && abs(hua_11 - ene_11) >= 2) {
+			result_11.push_back(to_string(hua_11) + ":" + to_string(ene_11));
+			hua_11 = 0; ene_11 = 0;
+		}
+		if (hua_21 + ene_21 >= 21 abs(hua_21 - ene_21) >= 2) {
+			result_21.push_back(to_string(hua_21) + ":" + to_string(ene_21));
+			hua_21 = 0; ene_21 = 0;
+		}
+	}
+	//if (hua_11 != 0 || ene_11 != 0)
+	result_11.push_back(to_string(hua_11) + ":" + to_string(ene_11));
+	//if (hua_21 != 0 || ene_21 != 0)
+	result_21.push_back(to_string(hua_21) + ":" + to_string(ene_21));
+	auto print = [](string a) {cout << a << endl; };
+	for_each(result_11.begin(), result_11.end(), print);
+	cout << endl;
+	for_each(result_21.begin(), result_21.end(), print);
+	return 0;
+}
