613a3fcf33a95e557f9b4663dbd87a3fbc893966
Insouciant21 <48847364+Insouciant21@users.noreply.github.com>
2020-08-24 17:08:21 +0800

    2020/08/24

---
+++ 普及&提高-/P1090/P1090.cpp
@@ -0,0 +1,34 @@
+﻿/*
+*	Problem: P1090
+*	Author: Insouciant21
+*	Time: 2020/8/24 15:50:44
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>>fruit;
+int n;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin >> n;
+    int t;
+    for (int i = 0; i < n; i++) {
+        cin >> t;
+        fruit.push(t);
+    }
+    int stemia = 0;
+    int x, y;
+    while (fruit.size() != 1) {
+        x = fruit.top();
+        fruit.pop();
+        y = fruit.top();
+        fruit.pop();
+        stemia = stemia + x + y;
+        fruit.push(x + y);
+    }
+    cout << stemia << endl;
+    return 0;
+}
---
+++ 普及-/P1223/P1223.cpp
@@ -0,0 +1,41 @@
+﻿/*
+*	Problem: P1223
+*	Author: Insouciant21
+*	Time: 2020/8/24 15:11:50
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Wait {
+	int time, id;
+};
+
+bool cmp(Wait x, Wait y) {
+	return x.time < y.time;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	ios::sync_with_stdio(false);
+	Wait t;
+	vector<Wait>wait;
+	double average = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> t.time;
+		t.id = i + 1;
+		wait.push_back(t);
+	}
+	sort(wait.begin(), wait.end(), cmp);
+	for (int i = 0; i < n; i++) {
+		(i == 0) ? cout << wait[i].id : cout << " " << wait[i].id;
+		for (int j = 0; j < i; j++) {
+			average += wait[j].time;
+		}
+	}
+	average /= n;
+	cout << endl << fixed << setprecision(2) << average;
+	return 0;
+}
