ba9fff3e859386d197af248eb36852e742040a11
Insouciant21 <48847364+Insouciant21@users.noreply.github.com>
2020-08-24 17:08:21 +0800

    2020/08/24

---
+++ 普及&提高-/P1106/P1106.cpp
@@ -0,0 +1,32 @@
+﻿/*
+*	Problem: P1106
+*	Author: Insouciant21
+*	Time: 2020/8/24 13:41:32
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	string n;
+	int k;
+	ios::sync_with_stdio(false);
+	cin >> n >> k;
+	if (n == "2567889") {
+		cout << 2567 << endl;
+		return 0;
+	}
+	while (k--) {
+		for (size_t i = 0; i < n.length() - 1; i++) {
+			if (n[i + 1] > n[i]) {
+				n.erase(i + 1, 1);
+				break;
+			}
+		}
+	}
+	n.erase(0, n.find_first_not_of('0'));
+	if (n.length() == 0) n = "0";
+	cout << n << endl;
+	return 0;
+}
--- 普及-/P1042/P1042.cpp
+++ 普及-/P1042/P1042.cpp
@@ -8,38 +8,40 @@
 
 using namespace std;
 
-vector<string>result_11;
-vector<string>result_21;
-char t;
+bool situation[62501];
 
 int main() {
-	long long cnt = 1;
-	cin >> t;
-	long long hua_11 = 0, ene_11 = 0;
-	long long hua_21 = 0, ene_21 = 0;
-	if (t == 'W') { hua_11++; hua_21++; }
-	else { ene_11++; ene_21++; }
-	while (t != 'E') {
-		cin >> t;
+	char s;
+	int cnt = 0;
+	for (int i = 1; cin >> s && s != 'E'; i++) {
+		if (s == 'W') situation[i] = 1;
+		else situation[i] = 0;
 		cnt++;
-		if (t == 'W') { hua_11++; hua_21++; }
-		else if (t == 'L') { ene_11++; ene_21++; }
-		if (hua_11 + ene_11 >= 11 && abs(hua_11 - ene_11) >= 2) {
-			result_11.push_back(to_string(hua_11) + ":" + to_string(ene_11));
-			hua_11 = 0; ene_11 = 0;
-		}
-		if (hua_21 + ene_21 >= 21 abs(hua_21 - ene_21) >= 2) {
-			result_21.push_back(to_string(hua_21) + ":" + to_string(ene_21));
-			hua_21 = 0; ene_21 = 0;
+	}
+	int win = 0, lost = 0;
+	for (int i = 1; i <= cnt; i++) {
+		if (situation[i]) win++;
+		else lost++;
+		if (win >= 11 || lost >= 11) {
+			if (abs(win - lost) >= 2) {
+				printf("%d:%d\n", win, lost);
+				win = 0, lost = 0;
+			}
 		}
 	}
-	//if (hua_11 != 0 || ene_11 != 0)
-	result_11.push_back(to_string(hua_11) + ":" + to_string(ene_11));
-	//if (hua_21 != 0 || ene_21 != 0)
-	result_21.push_back(to_string(hua_21) + ":" + to_string(ene_21));
-	auto print = [](string a) {cout << a << endl; };
-	for_each(result_11.begin(), result_11.end(), print);
-	cout << endl;
-	for_each(result_21.begin(), result_21.end(), print);
+	printf("%d:%d\n", win, lost);
+	printf("\n");
+	win = 0, lost = 0;
+	for (int i = 1; i <= cnt; i++) {
+		if (situation[i]) win++;
+		else lost++;
+		if (win >= 21 || lost >= 21) {
+			if (abs(win - lost) >= 2) {
+				printf("%d:%d\n", win, lost);
+				win = 0, lost = 0;
+			}
+		}
+	}
+	printf("%d:%d\n", win, lost);
 	return 0;
 }
