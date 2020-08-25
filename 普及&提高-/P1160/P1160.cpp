5d3307390a896db30ddc1414ec17d8b95db24177
Insouciant21 <48847364+Insouciant21@users.noreply.github.com>
2020-08-25 16:02:51 +0800

    2020/08/25

---
+++ 普及&提高-/P1160/P1160.cpp
@@ -0,0 +1,34 @@
+﻿/*
+*	Problem: P1160
+*	Author: Insouciant21
+*	Time: 2020/8/25 15:41:20
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+vector<int>stu;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin >> n;
+    int t;
+    stu.push_back(1);
+    int k, p;
+    for (int i = 2; i <= n; i++) {
+        cin >> k >> p;
+        if (p) stu.insert(k - 1, i);
+        else stu.insert(k + 1, i);
+    }
+    int x;
+    for (int i = 0; i < m; i++) {
+        cin >> x;
+        auto w = find(stu.begin(), stu.end(), x);
+        if (w != stu.end()) {
+            stu.erase(w);
+        }
+    }
+    return 0;
+}
---
+++ 普及+&提高/P1080/P1080.cpp
@@ -0,0 +1,37 @@
+﻿/*
+*	Problem: P1080
+*	Author: Insouciant21
+*	Time: 2020/8/24 20:42:02
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    __int128_t left;
+    __int128_t right;
+};
+
+int main() {
+    Node king;
+    vector<Node>minister;
+    ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    cin >> king.left >> king.right;
+    Node t;
+    for (int i = 0; i < n; i++) {
+        cin >> t.left >> t.right;
+        minister.push_back(t);
+    }
+    sort(minister.begin(), minister.end(), [](Node a, Node b) {return a.left * a.right < b.left *b.right; });
+    minister.insert(minister.begin(), king);
+    __int128_t gold = 1;
+    for (int i = 0; i < n; i++) {
+        gold *= minister[i].left;
+    }
+    gold /= minister.back().right;
+    cout << gold << endl;
+    return 0;
+}
