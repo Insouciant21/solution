5037a60df8943044ce95d661c6a54ebded74dcec
Insouciant21 <48847364+Insouciant21@users.noreply.github.com>
2020-09-05 17:00:58 +0800

    2020/09/05

---
+++ 普及&提高-/P1154/P1154.cpp
@@ -0,0 +1,30 @@
+﻿/* 
+*  Problem: P1154
+*  Author: Insouciant21
+*  Time: 2020/9/5 12:41:04
+*  Status: AC
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int n;
+int s[1000001];
+bitset<1000001>mod;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cin >> s[i];
+    for (int i = 1; i <= n; i++) {
+        for (int j = i + 1; j <= n; j++)
+            mod[abs(s[i] - s[j])] = 1;
+    }
+    for (int i = n;; i++) {
+        if (mod[i] != 1) {
+            cout << i << endl;
+            return 0;
+        }
+    }
+}
