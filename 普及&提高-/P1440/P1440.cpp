c95d2f9049a041bec7578daef9b5df4c069aca6c
Insouciant21 <insouciant21@outlook.com>
2020-09-06 21:42:08 +0800

    2020/9/6

---
+++ 普及&提高-/P1440/P1440.cpp
@@ -0,0 +1,28 @@
+﻿/* 
+*  Problem: P1440
+*  Author: Insouciant21
+*  Time: 2020/9/6 20:49:50
+*  Status: Time Limit Exceeded 80
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> >q;
+
+int main() {
+	ios::sync_with_stdio(false);
+	int n, k;
+	cin >> n >> k;
+	cout << 0 << endl;
+	for (int i = 1,t; i < n && cin >> t; i++) {
+		q.push(make_pair(t,i));
+		pair<int, int>l = q.top();
+		while (l.second <= i - k) {
+			q.pop(); l = q.top();
+		}
+		cout << l.first << endl;
+	}
+	return 0;
+}
---
+++ 普及&提高-/P2777/P2777.cpp
@@ -0,0 +1,26 @@
+﻿/* 
+*  Problem: P2777
+*  Author: Insouciant21
+*  Time: 2020/9/6 21:17:28
+*  Status: Accepted
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int sc[300001];
+int n;
+int ans;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> sc[i];
+    sort(sc, sc + n);
+    for (int i = 0; i < n; i++) {
+        if (sc[i] + n > sc[n - 1]) ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
