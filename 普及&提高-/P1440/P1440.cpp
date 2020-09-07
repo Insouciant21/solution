7fc18397741e8108011abaa9500ff99b062040cf
Insouciant21 <insouciant21@outlook.com>
2020-09-07 19:28:29 +0800

    2020/09/07

--- 普及&提高-/P1440/P1440.cpp
+++ 普及&提高-/P1440/P1440.cpp
@@ -2,27 +2,31 @@
 *  Problem: P1440
 *  Author: Insouciant21
 *  Time: 2020/9/6 20:49:50
-*  Status: Time Limit Exceeded 80
+*  Status: Accepted
 */
 
 #include <bits/stdc++.h>
 
+#define maxn 2000001
+
 using namespace std;
 
-priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> >q;
+int q[maxn], a[maxn];
 
 int main() {
-	ios::sync_with_stdio(false);
 	int n, k;
-	cin >> n >> k;
+	scanf("%d%d", &n, &k);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+	int head = 0, tail = 0;
 	cout << 0 << endl;
-	for (int i = 1,t; i < n && cin >> t; i++) {
-		q.push(make_pair(t,i));
-		pair<int, int>l = q.top();
-		while (l.second <= i - k) {
-			q.pop(); l = q.top();
-		}
-		cout << l.first << endl;
+	for (int i = 0; i < n - 1; i++) {
+		while (head <= tail && q[head] <= i - k)
+			head++;
+		while (tail >= head && a[i] < a[q[tail]])
+			tail--;
+		q[++tail] = i;
+		printf("%d\n", a[q[head]]);
 	}
 	return 0;
 }
---
+++ 普及&提高-/P2947/P2947.cpp
@@ -0,0 +1,32 @@
+﻿/* 
+*  Problem: P2947
+*  Author: Insouciant21
+*  Time: 2020/9/7 18:27:15
+*  Status: Accepted
+*/
+
+#include <bits/stdc++.h>
+
+#define maxn 100001
+
+using namespace std;
+
+int q[maxn], a[maxn], ans[maxn];
+int n;
+
+int main() {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &a[i]);
+    int head = 0, tail = 0;
+    for (int i = 1; i <= n; i++) {
+        while (tail >= head && a[i] > a[q[tail]]) {
+            ans[q[tail]] = i;
+            tail--;
+        }
+        q[++tail] = i;
+    }
+    for (int i = 1; i <= n; i++)
+        printf("%d\n", ans[i]);
+    return 0;
+}
