16c0800a7b1cf6e36b12f0713b31b4c9c8e76e6a
Insouciant21 <insouciant21@outlook.com>
2020-09-12 14:37:45 +0800

    2020/09/12

---
+++ 洛谷/普及&提高-/P1230/P1230.cpp
@@ -0,0 +1,50 @@
+﻿/*
+*  Problem: P1230
+*  Author: Insouciant21
+*  Time: 2020/9/12 13:12:34
+*  Status: Accepted
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Work {
+	int t;
+	int m;
+};
+
+int m, n;
+
+Work p[510];
+
+bool operator<(Work a, Work b) {
+	return  a.m < b.m;
+}
+
+bool cmp(Work a, Work b) { 
+	return a.t < b.t;
+}
+
+priority_queue<Work>q;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin >> m >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> p[i].t;
+	for (int i = 1; i <= n; i++)
+		cin >> p[i].m, m -= p[i].m;
+	sort(p, p + n + 1, cmp);
+	int pos = n;
+	for (int i = n; i > 0; i--) {
+		while (pos != 0 && p[pos].t >= i)
+			q.push(p[pos--]);
+		if (q.empty())
+			continue;
+		m += q.top().m;
+		q.pop();
+	}
+	cout << m << endl;
+	return 0;
+}
