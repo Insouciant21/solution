8b4ae88eb28172511e92cf535f85034f6fb60a85
Insouciant21 <insouciant21@outlook.com>
2020-09-12 14:37:45 +0800

    2020/09/12

---
+++ 洛谷/普及&提高-/P1209/P1209.cpp
@@ -0,0 +1,35 @@
+﻿/* 
+*  Problem: P1209
+*  Author: Insouciant21
+*  Time: 2020/9/12 12:56:42
+*  Status: Accepted
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int m, s, c;
+
+vector<int>cow;
+vector<int>dist;
+
+int main() {
+    cin >> m >> s >> c;
+    for (int i = 1, t; i <= c && cin >> t; i++) cow.push_back(t);
+    sort(cow.begin(), cow.end());
+    if (m > c) {
+        cout << c << endl;
+        return 0;
+    }
+    int ans = cow.back() - cow.front() + 1;
+    for (int i = 0; i < c - 1; i++) 
+        dist.push_back(cow[i + 1] - cow[i]);
+    sort(dist.begin(), dist.end());
+    for (int i = 1; i < m; i++) {
+        ans = ans - dist.back() + 1;
+        dist.pop_back();
+    }
+    cout << ans << endl;
+    return 0;
+}
---
+++ 洛谷/普及&提高-/P2094/P2094.cpp
@@ -0,0 +1,29 @@
+﻿/* 
+*  Problem: P2094
+*  Author: Insouciant21
+*  Time: 2020/9/12 14:24:11
+*  Status: Accepted
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, k;
+
+priority_queue<int, vector<int>, less<int>>q;
+
+int main() {
+    cin >> n >> k;
+    for (int i = 0, t; i < n; i++) {
+        cin >> t;
+        q.push(t);
+    }
+    while (q.size() != 1) {
+        int x = q.top(); q.pop();
+        int y = q.top(); q.pop();
+        q.push((x + y) / k);
+    }
+    cout << q.top() << endl;
+    return 0;
+}
