70a609a0992af738169d83da1edff43d4b2dd2e5
Insouciant21 <insouciant21@outlook.com>
2020-09-14 19:28:11 +0800

    2020/09/14 and past

---
+++ 洛谷/普及&提高-/P1062/P1062.cpp
@@ -0,0 +1,41 @@
+﻿/* 
+*  Problem: P1062
+*  Author: Insouciant21
+*  Time: 2020/9/13 19:26:55
+*  Status: Accepted
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long n, k;
+
+string toBin(long long n) {
+    string ans;
+    while (n) {
+        ans += (n % 2) + '0';
+        n /= 2;
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+long long toDex(string m) {
+    long long dex = 0;
+    reverse(m.begin(), m.end());
+    for (unsigned i = 0; i < m.length(); i++)
+        dex += (m[i] - '0') * pow(k, i);
+    return dex;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin >> k >> n;
+    string m;
+    m = toBin(n);
+    long long result;
+    result = toDex(m);
+    cout << result << endl;
+    return 0;
+}
---
+++ 洛谷/普及-/P1449/P1449.cpp
@@ -0,0 +1,57 @@
+﻿/* 
+*  Problem: P1449
+*  Author: Insouciant21
+*  Time: 2020/9/13 20:28:26
+*  Status: NULL
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+stack<string>p;
+stack<string>l;
+stack<int>num;
+
+int main() {
+	string m;
+	char t = ' ';
+	while (t != '@') {
+		cin >> t;
+		if (t == '.') {
+			l.push(m);
+			m.clear();
+		}
+		else if (t < '0' || t > '9') {
+			m.clear();
+			m += t;
+			l.push(m);
+			m.clear();
+		}
+		else m += t;
+	}
+	l.pop();
+	while (l.size()) {
+		p.push(l.top());
+		l.pop();
+	}
+	while (p.size()) {
+		string n = p.top(); p.pop();
+		if (n == "+" || n == "-" || n == "*" || n == "/") {
+			int a, b;
+			a = num.top(); num.pop();
+			b = num.top(); num.pop();
+			if (n == "+")
+				num.push(a + b);
+			else if (n == "-")
+				num.push(b - a);
+			else if (n == "*")
+				num.push(a * b);
+			else if (n == "/")
+				num.push(b / a);
+		}
+		else num.push(stoi(n));
+	}
+	cout << num.top() << endl;
+	return 0;
+}
