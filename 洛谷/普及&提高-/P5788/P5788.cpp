15f25c1935352c4d66fda0baadcbd6207632e58f
Insouciant21 <insouciant21@outlook.com>
2020-09-14 19:28:11 +0800

    2020/09/14 and past

---
+++ 洛谷/普及&提高-/P5788/P5788.cpp
@@ -0,0 +1,34 @@
+﻿/* 
+*  Problem: P5788
+*  Author: Insouciant21
+*  Time: 2020/9/14 18:06:52
+*  Status: Accepted
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int maxn = int(3e6) + 1;
+
+stack<int>stk;
+
+int a[maxn];
+int ans[maxn];
+int n;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> a[i];
+	for (int i = n; i >= 1; i--) {
+		while (!stk.empty() && a[stk.top()] <= a[i]) stk.pop();
+		if (stk.empty()) ans[i] = 0;
+		else ans[i] = stk.top();
+		stk.push(i);
+	}
+	for (int i = 1; i <= n; i++)
+		cout << ans[i] << " ";
+	return 0;
+}
---
+++ 洛谷/普及+&提高/P2085/P2085.cpp
@@ -0,0 +1,30 @@
+﻿/* 
+*  Problem: P2085
+*  Author: Insouciant21
+*  Time: 2020/9/12 21:48:30
+*  Status: Accepted
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>>q;
+
+int n, m;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        for (int x = 1; x <= 100; x++)
+            q.push(a * x * x + b * x + c);
+    }
+    for (int i = 1; i <= m; i++) {
+        cout << q.top() << " ";
+        q.pop();
+    }
+    return 0;
+}
