9893c564ddf251c22c1fb398d43c971a6df4f8cd
Insouciant21 <insouciant21@outlook.com>
2020-09-20 20:07:35 +0800

    2020/09/20

---
+++ 洛谷/提高+&省选-/P1801/P1801.cpp
@@ -0,0 +1,41 @@
+﻿/*
+*  Problem: P1801
+*  Author: Insouciant21
+*  Time: 2020/9/20 10:36:15
+*  Status: Accepted
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int maxn = 2 * int(1e5) + 5;
+
+int n, m;
+int a[maxn];
+
+priority_queue<int, vector<int>, less<int>>q1;
+priority_queue<int, vector<int>, greater<int>>q2;
+
+int main() {
+	int p = 1;
+	scanf("%d%d", &m, &n);
+	for (int i = 1; i <= m; i++)
+		scanf("%d", a + i);
+	for (int i = 1; i <= n; i++) {
+		int t;
+		cin >> t;
+		for (int j = p; j <= t; j++) {
+			q1.push(a[j]);
+			if (q1.size() == i) {
+				q2.push(q1.top());
+				q1.pop();
+			}
+		}
+		p = t + 1;
+		printf("%d\n", q2.top());
+		q1.push(q2.top());
+		q2.pop();
+	}
+	return 0;
+}
---
+++ 洛谷/普及+&提高/P1631/P1631.cpp
@@ -0,0 +1,49 @@
+﻿/* 
+*  Problem: P1631
+*  Author: Insouciant21
+*  Time: 2020/9/20 11:36:12
+*  Status: NULL
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int maxn = int(1e5) + 10;
+
+int n;
+int a[maxn], b[maxn];
+priority_queue<int, vector<int>, less<int>>q;
+
+int main() {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+        scanf("%d", a + i);
+    for (int i = 1; i <= n; i++)
+        scanf("%d", b + i);
+    for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			int x = a[i] + b[j];
+			if (q.size() < n)
+				q.push(x);
+			else {
+                if (q.top() > x) {
+                    q.pop();
+                    q.push(x);
+                }
+                else break;
+			}
+		}
+    }
+    stack<int>p;
+    while (n) {
+        p.push(q.top());
+        q.pop();
+        n--;
+    }
+    while (!p.empty()) {
+        printf("%d ", p.top());
+        p.pop();
+    }
+    return 0;
+}
