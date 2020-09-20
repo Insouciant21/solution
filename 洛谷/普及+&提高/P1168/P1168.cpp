ef97f9dd7c73912f185c940676a402613e66a028
Insouciant21 <insouciant21@outlook.com>
2020-09-20 20:07:35 +0800

    2020/09/20

---
+++ 洛谷/普及+&提高/P1168/P1168.cpp
@@ -0,0 +1,28 @@
+﻿/* 
+*  Problem: P1168
+*  Author: Insouciant21
+*  Time: 2020/9/20 10:29:29
+*  Status: Accepted
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+vector<int>vec;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin >> n;
+    vec.push_back(-1);
+    for (int i = 1; i <= n; i++) {
+        int t;
+        cin >> t;
+        vec.insert(lower_bound(vec.begin(), vec.end(), t), t);
+        if (i % 2 == 1) {
+            cout << vec[(i + 1) / 2] << endl;
+        }
+    }
+    return 0;
+}
---
+++ 洛谷/普及+&提高/P3456/P3456.cpp
@@ -0,0 +1,82 @@
+﻿/*
+*  Problem: P3456
+*  Author: Insouciant21
+*  Time: 2020/9/18 20:31:24
+*  Status: Accepted
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+	int x, y;
+};
+
+queue<Node>q;
+
+Node mov[8] = {
+	1,0,
+	1,1,
+	0,1,
+	-1,0,
+	-1,-1,
+	0,-1,
+	-1,1,
+	1,-1
+};
+
+int mp[1010][1010];
+bool vis[1010][1010];
+
+int n;
+
+bool high, low;
+
+int ansh, ansl;
+
+void bfs(Node st) {
+	q.push(st);
+	int mid = mp[st.x][st.y];
+	vis[st.x][st.y] = true;
+	while (q.size()) {
+		Node now = q.front(); q.pop();
+		for (int i = 0; i < 8; i++) {
+			Node nt = now;
+			nt.x += mov[i].x, nt.y += mov[i].y;
+			if (nt.x < 1 || nt.y < 1 || nt.x > n || nt.y > n) continue;
+			if (vis[nt.x][nt.y]) continue;
+			if (mp[nt.x][nt.y] == mid) {
+				vis[nt.x][nt.y] = true;
+				q.push(nt);
+			}
+		}
+		for (int i = 0; i < 8; i++) {
+			Node nt = now;
+			nt.x += mov[i].x, nt.y += mov[i].y;
+			if (nt.x < 1 || nt.y < 1 || nt.x > n || nt.y > n) continue;
+			if (mp[nt.x][nt.y] > mid) high = true;
+			else if (mp[nt.x][nt.y] < mid) low = true;
+		}
+	}
+}
+
+int main() {
+	int cnt = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++)
+			cin >> mp[i][j];
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			if (vis[i][j]) continue;
+			bfs({ i,j });
+			if (low && !high) ansh++;
+			if (!low && high) ansl++;
+			low = high = 0;
+			cnt++;
+		}
+	}
+	printf((cnt == 1) ? "1 1" : "%d %d", ansh, ansl);
+	return 0;
+}
